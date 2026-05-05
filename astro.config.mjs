78ba481ea91b2a73c85af8857da7fd0417999989
Arjan <ahoekdev@gmail.com>
2026-05-05 21:25:17 +0200

    install supabase ssr package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
-  integrations: [react()]
+  integrations: [react()],
 });
