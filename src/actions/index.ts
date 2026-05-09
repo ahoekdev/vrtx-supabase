db4d9e1d587e56c4cf266baad6767fd6b0c224d9
Arjan <ahoekdev@gmail.com>
2026-05-09 06:50:46 +0200

    add login and register actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,11 @@
+import { loginAction } from "./auth/loginAction";
+import { registerAction } from "./auth/registerAction";
 import { createLodgeAction } from "./lodge/createLodgeAction";
 import { deleteLodgeAction } from "./lodge/deleteLodgeAction";
 
 export const server = {
   createLodge: createLodgeAction,
   deleteLodge: deleteLodgeAction,
+  login: loginAction,
+  register: registerAction,
 };
