5455028416dff5b2a29c313b7f7194dc1b58e60e
Arjan Hoek <ahoekdev@gmail.com>
2026-05-31 18:45:48 +0200

    remove unused components

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,7 @@
 import { loginAction } from "./auth/loginAction";
 import { registerAction } from "./auth/registerAction";
-import { createLodgeAction } from "./lodge/createLodgeAction";
-import { deleteLodgeAction } from "./lodge/deleteLodgeAction";
 
 export const server = {
-  createLodge: createLodgeAction,
-  deleteLodge: deleteLodgeAction,
   login: loginAction,
   register: registerAction,
 };
