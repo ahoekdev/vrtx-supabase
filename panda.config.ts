3780fc2ceaea9837fcf76e12909c9729bcb714dd
Arjan Hoek <ahoekdev@gmail.com>
2026-06-05 06:33:06 +0200

    move typograpy css to panda

--- panda.config.ts
+++ panda.config.ts
@@ -1,20 +1,57 @@
-import { defineConfig } from "@pandacss/dev";
+import {
+  defineConfig,
+  defineGlobalStyles,
+} from "@pandacss/dev";
+
+const bodyTypography = {
+  fontFamily:
+    '"Lucida Sans", "Lucida Sans Regular", "Lucida Grande", "Lucida Sans Unicode", Geneva, Verdana, sans-serif',
+};
+
+const headingTypography = {
+  fontWeight: "bold",
+};
+
+const globalCss = defineGlobalStyles({
+  body: {
+    ...bodyTypography,
+  },
+  h1: {
+    fontSize: "2rem",
+    marginTop: "0.5rem",
+    marginBottom: "1.5rem",
+    ...headingTypography,
+  },
+  h2: {
+    fontSize: "1.5rem",
+    marginTop: "0.5rem",
+    marginBottom: "0.25rem",
+    ...headingTypography,
+  },
+  h3: {
+    fontSize: "1.25rem",
+    marginTop: "0.5rem",
+    marginBottom: "0.25rem",
+    ...headingTypography,
+  },
+});
 
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
-
-  // Where to look for your css declarations
+  eject: true,
+  globalCss,
   include: ["./src/**/*.{ts,tsx,js,jsx,astro}"],
-
-  // Files to exclude
-  exclude: [],
-
-  // Useful for theme customization
-  theme: {
-    extend: {},
+  presets: ["@pandacss/preset-base"],
+  utilities: {
+    color: {
+      values: "colors",
+    },
   },
-
-  // The output directory for your css system
+  theme: {
+    tokens: {
+      colors: {},
+    },
+  },
   outdir: "styled-system",
 });
