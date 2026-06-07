8db8f5a6cafe64c4ee684b37be8935ba31e841f0
Arjan Hoek <ahoekdev@gmail.com>
2026-06-07 12:15:28 +0200

    add set favorite action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,9 +1,11 @@
 import { loginAction } from "./auth/loginAction";
 import { registerAction } from "./auth/registerAction";
 import { signoutAction } from "./auth/signoutAction";
+import { setFavoriteAction } from "./lodges/setFavoriteAction";
 
 export const server = {
   login: loginAction,
   register: registerAction,
   signout: signoutAction,
+  setFavorite: setFavoriteAction,
 };
